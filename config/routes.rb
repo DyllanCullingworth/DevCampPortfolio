7bb2ed04b72bdcad22318cb556ed2b0b70f870f4
mmdyllan@gmail.com <mmdyllan@gmail.com>
2020-11-24 16:13:00 +0200

    added portfolio items via resources generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
+
   resources :blogs
+  resources :portfolios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
