7557709000b1e9815b3e3a424b16b3e7b74c7642
mmdyllan@gmail.com <mmdyllan@gmail.com>
2020-11-25 12:35:57 +0200

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ puts "5 skills created"
     subtitle: "My great service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://via.placeholder.com/600x400/#{SecureRandom.hex(3)}/000000?Text=#{portfolio_item}",
-    thumb_image: "https://via.placeholder.com/350x200"
+    thumb_image: "https://via.placeholder.com/350x200/#{SecureRandom.hex(3)}/000000?Text=#{portfolio_item}"
   )
 end
 
