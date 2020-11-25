f89e34afbcdb1c7540cbff86f2981c015e1d5183
mmdyllan@gmail.com <mmdyllan@gmail.com>
2020-11-25 13:19:13 +0200

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,30 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    if @portfolio_item.save
+      flash[:notice] = "Success"
+      redirect_to portfolios_path
+    else
+      flash[:alert]
+      render new
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image )
+  end
+
 end
