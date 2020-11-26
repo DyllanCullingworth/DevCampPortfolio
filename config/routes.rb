c87f3a826ad3fa3a182809c3f7442db498e05da5
mmdyllan@gmail.com <mmdyllan@gmail.com>
2020-11-26 16:32:02 +0200

    Customized devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # ================= Pages
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  # == Pages
   root to: 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  # =================
+  # ==
 
-  # ================= Blogs
+  # == Blogs
   resources :blogs do
     member do
       get :toggle_status
     end
   end
 
-  # =================
+  # ==
 
-  # ================= Portfolios
+  # == Portfolios
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
   get 'angular', to: 'portfolios#angular'
-  # =================
+  # ==
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
