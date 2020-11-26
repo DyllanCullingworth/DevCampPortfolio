b81b5ea3fcaf9349ee207806c8feb7d6279569b4
mmdyllan@gmail.com <mmdyllan@gmail.com>
2020-11-26 14:44:58 +0200

    integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,8 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  has_many :technologies
+
   def self.angular
     where(subtitle: 'Angular')
   end
