b33033d83f8a06eae3613d377066e63dbbfff9d6
mmdyllan@gmail.com <mmdyllan@gmail.com>
2021-01-07 15:48:01 +0200

    implimented authorization engine for blogs and portfolios

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def login_helper
-    if current_user.is_a?(User)
+    unless current_user.is_a?(GuestUser)
       link_to("Logout", destroy_user_session_path, method: :delete)
     else
       (link_to("Login", new_user_session_path)) +
